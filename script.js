8ffa3637e9fa8ca2358b19ba51b89585549319e2
Muammad Shaheer <shaheerkhan0012345@gmail.com>
2026-06-05 13:02:04 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,96 @@
+                     // Print 1 TO 100
+
+
+// for(let i=1; i< 100; i++){
+//     console.log(i)
+// }
+
+
+
+
+// Print Even Numbers
+
+
+// for(let i=2; i<100; i++){
+
+//     if (i % 2 === 0) {
+//         console.log(i)
+//     }
+// }
+
+
+
+
+// Sum of 1 to 100
+
+// let sum = 0;
+
+
+// for(let i=1; i < 100;i++){
+//     sum += i
+// }
+
+// console.log(sum)
+
+
+
+
+// Multiplication Table of 7
+
+// let sum = 0
+
+// for(let i =1; i<=10;i++){
+
+//    console.log(`7 *${i} = ${7 * i}`);
+
+// }
+
+
+
+// Reverse Counting
+
+
+// for(let i=100; i> 1;i--){
+//     console.log(i);
+// }
+
+
+
+
+// Count Vowels
+                //First Way
+// let str = "shaheer";
+// let count = 0
+
+
+// for(let i = 0; i < str.length; i++){
+//     let ch = str[i];
+
+//     if( 
+//         ch == "a" ||
+//         ch == "e" ||
+//         ch == "i" ||
+//         ch == "o" ||
+//         ch == "u"
+//     ){
+//     count++;
+//     }
+// }
+// console.log(count);
+
+
+            //Second way
+// let str = "Zunaira"
+// let count = 0
+
+
+// for(let ch of str){
+//     if("aeiou".includes(ch)){
+//         count++;
+//     }
+// }
+// console.log(count);
+
+
+
+// Smallest Number in Array
