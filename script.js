33f4cad2fb4d7759c5f9f0b8c9f1f86d7dddfa57
Muammad Shaheer <shaheerkhan0012345@gmail.com>
2026-06-06 09:51:03 -0700

    Day 2  JavaScript practice

--- script.js
+++ script.js
@@ -59,6 +59,7 @@
 
 // Count Vowels
                 //First Way
+
 // let str = "shaheer";
 // let count = 0
 
@@ -80,6 +81,7 @@
 
 
             //Second way
+
 // let str = "Zunaira"
 // let count = 0
 
@@ -93,4 +95,109 @@
 
 
 
+
+
+
+
+
+
+
+// Largest Number in Array
+
+
+// let  num = [400,300,200,100,50];
+// let largest = num[0];
+
+
+// for(let numbers of num){
+
+//     if(numbers > largest){
+//         largest = numbers
+//     }
+
+// }
+// console.log(largest);
+
+
+
+
 // Smallest Number in Array
+
+// let num = [200,50,30,20,10]
+
+// let smallest = num[0];
+
+
+// for(let numbers of num){
+//     if(numbers < smallest){
+//         smallest = numbers
+//     }
+// }
+
+// console.log(smallest)
+
+
+
+// Sum of Array
+
+
+// let arr = [100,150,200,350];
+
+// let sum = 0;
+
+// for(let i=0; i < arr.length; i++){
+//      sum = sum + arr[i];
+// }
+
+// console.log(sum);
+
+
+
+// Reverse String
+
+// let str = "Shaheer"
+// let reverse = "";
+
+
+// for(let i = str.length - 1; i >=0; i-- ){
+//     reverse += str[i];
+// }
+
+// console.log(reverse);
+
+
+
+// Count Positive Numbers
+
+// let arr = [100,200,50,70,150];
+
+
+// for(let i=0; i < arr.length; i++){
+
+//     if(arr[i] > 0){
+
+//          console.log(arr[i]);
+//     }
+
+// }
+
+
+
+// Find Average
+
+
+let num = [10,20,30,40,50];
+
+let sum = 0;
+
+for(let numbers of num){
+
+    sum += numbers;
+}
+
+let avg = sum / num.length;
+
+console.log(avg);
+
+
+
